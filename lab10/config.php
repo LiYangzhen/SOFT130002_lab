7d40b149af572c665bae16917073241d3fa89299
LiYangzhen <1345356001@qq.com>
2020-06-07 18:05:09 +0800

    hand lab10

---
+++ lab10/config.php
@@ -0,0 +1,6 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testuser');
+define('DBPASS', '555100200');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
