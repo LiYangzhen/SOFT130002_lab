adb086a11436b02e564829d86cbcfac3f91dbd7a
LiYangzhen <1345356001@qq.com>
2020-06-11 16:38:38 +0800

    hand lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'testuser');
+define('DBPASS', '555100200');
+define('DBCONNSTRING','mysql:dbname=login;charset=utf8mb4;');
+?>
